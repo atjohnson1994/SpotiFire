fee6f250af01c1cf03e1d888809b5eeb58b7a9d9
atjohnson1994 <atjohnson1994@gmail.com>
2024-06-28 10:22:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ############################# Set up ###########################################
 # Import Flask dependencies
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
